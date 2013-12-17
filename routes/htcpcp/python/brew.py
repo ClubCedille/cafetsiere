da0fb4b5e1d74df9e2f275d5b76ef72d9f06f0f9
Riku Avelar <riku.avelar@gmail.com>
2013-12-17 18:54:44 -0500

    Attempt at daemonized Python script

--- routes/htcpcp/python/brew.py
+++ routes/htcpcp/python/brew.py
@@ -1,14 +1,36 @@
 try: import simplejson as json
 except ImportError: import json
 import sys
+from daemon import Daemon
 
-def brew(coffee):
-	coffee = json.loads(coffee)
-	return "We are now brewing {} Cups of delicious".format(coffee['cups'])
+class CoffeeDaemon(Daemon):
+
+	def brew(coffee):
+		coffee = json.loads(coffee)
+		return "We are now brewing {} Cups of delicious".format(coffee['cups'])
+	
+	def stop(coffee):
+		coffee = json.loads(coffee)
+		return "We are now brewing {} Cups of delicious".format(coffee['cups'])
+
+
+if __name__ == "__main__":
+	daemon = CoffeeDaemon('/tmp/coffee-daemon.pid')
+	if len(sys.argv) == 2 || len(sys.argv) == 3:
+		if 'brew' == sys.argv[1]:
+			if len(sys.argv) == 3:
+				daemon.brew()
+			else:
+				print 'Missing JSON data'
+				sys.exit(2)
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		else:
+			print 'Unknown Command'
+			sys.exit(2)
+		sys.exit(0)
+	else :
+		print "Usage: %s brew|stop [json data]" sys.argv[0]
+		sys.exit(2)
 
 # lambda x: brew(x)
-
-if(len(sys.argv) >= 2):
-	print(brew(sys.argv[1]))
-else:
-	print('Error')
